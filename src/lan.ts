14e90490a2d5c6b87732e39f68783ff11b5bcab9
Rooted <modhanami@gmail.com>
2022-08-11 10:55:36 +0700

    Refactor lan.ts

--- src/lan.ts
+++ src/lan.ts
@@ -1,16 +1,16 @@
-import { networkInterfaces } from 'os';
+import { NetworkInterfaceInfoIPv4, networkInterfaces } from 'os';
 
-function belongsToSubnet(ip, mask, subnet) {
-  const ipBytes = ip.split('.').map(octet => parseInt(octet));
-  const subnetBytes = subnet.split('.').map(octet => parseInt(octet));
-  const maskBytes = mask.split('.').map(octet => parseInt(octet));
+function belongsToSubnet(ip: string, mask: string, subnet: string): boolean {
+  const ipBytes = ip.split('.').map((octet) => parseInt(octet));
+  const subnetBytes = subnet.split('.').map((octet) => parseInt(octet));
+  const maskBytes = mask.split('.').map((octet) => parseInt(octet));
 
-  return ipBytes.every((ipByte, i) => {
+  return ipBytes.every((ipByte: number, i: string | number) => {
     return (ipByte & maskBytes[i]) === subnetBytes[i];
   });
 }
 
-export function getLANIP(subnet) {
+export function getLANIP(subnet: string): string {
   const interfaces = getIPv4Interfaces();
 
   const lanInterface = interfaces.find(int => {
@@ -20,11 +20,19 @@ export function getLANIP(subnet) {
     return belongsToSubnet(ip, netmask, subnet);
   });
 
+  if (!lanInterface) {
+    throw new Error('No LAN interface found');
+  }
+
   return lanInterface.address;
 }
 
-function getIPv4Interfaces() {
-  return Object.values(networkInterfaces())
+function getIPv4Interfaces(): NetworkInterfaceInfoIPv4[] {
+  const interfaces = networkInterfaces();
+  const addresses = Object.values(interfaces)
     .flat()
-    .filter(int => int.family === 'IPv4');
+    .filter((int): int is NetworkInterfaceInfoIPv4 => int !== undefined);
+  const ipv4Addresses = addresses.filter(int => int.family === 'IPv4');
+
+  return ipv4Addresses;
 }
